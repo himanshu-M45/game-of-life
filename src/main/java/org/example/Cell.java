392ea474a752e34632d562b4655bafc66e6c093c
Himanshu Maharshi <himanshu.m_ftc@external.swiggy.in>
2024-09-26 16:14:37 +0530

    generated grid and initialized grid with ramdom 0's and 1's

--- src/main/java/org/example/Cell.java
+++ src/main/java/org/example/Cell.java
@@ -1,4 +1,13 @@
 package org.example;
 
+import org.example.enums.State;
+
 public class Cell {
+    public final int value;
+    private State state;
+
+    public Cell (int value) {
+        this.value = value;
+        this.state = State.DEAD;
+    }
 }
--- src/test/java/org/example/GridTest.java
+++ src/test/java/org/example/GridTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GridTest {
+    Grid grid = new Grid(10, 20);
+
+    @Test
+    void tesGridGenerationAndPrintGrid() {
+        assertDoesNotThrow(grid::printGrid);
+    }
 
 }
